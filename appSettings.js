ba8779fdf11c3f9f626376422aa4564f826fb407
chris-schmitz <schmitz.chris@gmail.com>
2016-10-08 22:06:57 -0500

    Adds environment start tool.

--- appSettings.js
+++ appSettings.js
@@ -1,9 +1,33 @@
 const path = require('path')
+const fs = require('fs')
 
-const appPaths= {
+/*=====================
+| Environment settings
+ =====================*/
+
+const appPaths = {
     environmentBuildDirectory: path.resolve(__dirname, '../AEMEnvironments')
 }
 
+const availableEnvironments = ['author', 'publish']
+
+function isAValidEnvironment(env){
+    return availableEnvironments.includes(env)
+}
+
+/*=====================
+| AEM Settings
+ =====================*/
+// The double backslash seems goofy when defining this regex, but remember that when we define it here
+// we're defining it _as a string_ to be turned into a regular expression later via `new RegExp`, so back
+// slash does what it does for a string (i.e. strike the next char) instead of what it does within a regular expression.
+const jarPattern = `aem-(${availableEnvironments.join('|')})-[\\d]+.jar`
+const requiredAemFilePatterns  = [jarPattern,'license\.properties']
+
+//======================
+//
 module.exports = {
-    appPaths
+    appPaths,
+    isAValidEnvironment,
+    requiredAemFilePatterns
 }
--- bin/aemhelper-start.js
+++ bin/aemhelper-start.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const fs = require('fs')
 const chalk = require('chalk')
+const co = require('co')
 const {exec} = require('child_process')
+const {appPaths, isAValidEnvironment} = require('../appSettings')
+const {directoryContainsRequiredAEMFiles,openJarFile} = require('../lib/FileSystemTools')
 
 program
-    .option('-e, --environment, The name of the environment you would like to start.')
+    .option('-e, --environment <env>, The name of the environment you would like to start.')
     .parse(process.argv)
 
-const env
+if(!isAValidEnvironment(program.environment)){
+    console.error(chalk.red(`The argument ${program.environment} is not a valid environment name.`))
+    return
+}
+
+co(function *(){
+    const targetPath = `${appPaths.environmentBuildDirectory}/${program.environment}`
+    let hasFiles = yield directoryContainsRequiredAEMFiles(targetPath)
+    yield openJarFile(`${targetPath}/aem-${program.environment}-*.jar`)
+    console.log(chalk.green(`AEM ${program.environment} enviornment starting up...`))
+})
+.catch(error => {throw new Error(chalk.red(error))})
