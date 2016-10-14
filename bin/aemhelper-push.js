096ab4dce974f2630797c93050e8ec441a74692d
Mike Wyatt II <mwyatt@paradowski.com>
2016-10-14 17:14:31 -0500

    Add watch method and extract push function into lib FileSystemTools

--- bin/aemhelper-push.js
+++ bin/aemhelper-push.js
@@ -3,21 +3,16 @@
 const program = require('commander')
 const {exec} = require('child_process')
 const chalk = require('chalk')
+const {pushToAEM} = require('../lib/FileSystemTools')
 
 program
-    .option('-p, --port [port], Port to use for push. Defaults to 4502.')
-    .option('-c, --credentials [user:password], Credentials to use for push.')
+    .option('-p, --port [port]', 'Port to use for push. Defaults to 4502.')
+    .option('-c, --credentials [user:password]', 'Credentials to use for push.')
     .parse(process.argv)
 
 let credentials = program.credentials ? `--credentials ${program.credentials}` : ''
 let port = program.port || '4502'
 
-let command = `vlt ${credentials} import http://localhost:${port}/crx ./ /`
-console.log(command)
-
-exec(command, (err, stdout, stderr) => {
-    if(err) throw new Error(chalk.red(err))
-    // if(stderr) throw new Error(chalk.red(stderr))
-
-    console.log(chalk.green(stdout))
-})
+pushToAEM(credentials, port)
+    .then(stdout => console.log(chalk.green(stdout)))
+    .catch(err => console.error(chalk.red(err)))
---
+++ bin/aemhelper-watch.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const program   = require('commander')
+const {exec}    = require('child_process')
+const chalk     = require('chalk')
+const gulp      = require('gulp')
+const {pushToAEM} = require('../lib/FileSystemTools')
+
+program
+    .option('-p, --port [port]', 'Port to use for push. Defaults to 4502.')
+    .option('-c, --credentials [user:password]', 'Credentials to use for push.')
+    .parse(process.argv)
+
+let credentials = program.credentials ? `--credentials ${program.credentials}` : ''
+let port = program.port || '4502'
+
+gulp.watch(['jcr_root/**/*','!jcr_root/**/.vlt', 'jcr_root/**/.content.xml'], 
+    () => {
+        pushToAEM(credentials, port)
+            .then(stdout => console.log(chalk.green(stdout)))
+            .catch(err => console.error(chalk.red(err)))
+        console.log(chalk.green("Gulp's watch has begun. Gulp is the shield that guards the realms of AEM."))
+    }
+)
+    .on('change', event => console.log('File ' + event.path + ' was ' + event.type))
