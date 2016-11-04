6ef5d06e0c5471735f0d170af6c331025013e8ae
Mike Wyatt II <mwyatt@paradowski.com>
2016-11-04 11:51:02 -0500

    Add option to compile sass only

--- bin/aemhelper-watch.js
+++ bin/aemhelper-watch.js
@@ -13,30 +13,36 @@ const fst = new FileSystemTools
 program
     .option('-p, --port [port]', 'Port to use for push. Defaults to 4502.', /[0-9]+/, 4502)
     .option('-c, --credentials [user:password]', 'Credentials to use for push.')
+    .option('--sassonly', 'Only compile sass', false)
     .parse(process.argv)
 
 
 
 let credentials = program.credentials ? `--credentials ${program.credentials}` : ''
+let sassOnly = program.sassonly;
 
-gulp.watch(['jcr_root/**/*','!jcr_root/**/.vlt', '!jcr_root/**/*.scss', 'jcr_root/**/.content.xml'],
-    () => {
-        fst.pushToAEM(credentials, program.port)
-            .then(stdout => logAndNotifiy(stdout, 'success', 'both'))
-            .catch(err => logAndNotifiy(stdout, 'failure', 'both'))
-    }
-)
-    .on('change', event => console.log('File ' + event.path + ' was ' + event.type))
+if (!sassOnly) {
+    gulp.watch(['jcr_root/**/*','!jcr_root/**/.vlt', '!jcr_root/**/*.scss', 'jcr_root/**/.content.xml'],
+        () => {
+            fst.pushToAEM(credentials, program.port)
+                .then(stdout => logAndNotifiy(stdout, 'success', 'both'))
+                .catch(err => logAndNotifiy(stdout, 'failure', 'both'))
+        }
+    )
+        .on('change', event => console.log('File ' + event.path + ' was ' + event.type))
+}
 
 gulp.watch(['jcr_root/**/*.scss'],
 	() => {}
 )
     .on('change', (file) => {
         gulp.src(file.path, {base: 'jcr_root'})
-            .pipe(sass())
+            .pipe(sass({indentWidth: 2}))
             .on('error', sass.logError)
             .pipe(gulp.dest('jcr_root/'))
 
+        logAndNotifiy([file.path,' was compiled'].join(''), 'success', 'both')
+
     })
 
-console.log(chalk.green("Gulp's watch has begun. Gulp is the shield that guards the realms of AEM."))
+logAndNotifiy("Gulp's watch has begun. Gulp is the shield that guards the realms of AEM.", 'success', 'both')
