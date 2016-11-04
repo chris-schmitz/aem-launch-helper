21df851f54d7770574fcfce700c964f1a28eb4ee
Mike Wyatt II <mwyatt@paradowski.com>
2016-11-04 13:05:28 -0500

    Add sass only option and compile only the changed SCSS file

--- bin/aemhelper-watch.js
+++ bin/aemhelper-watch.js
@@ -12,28 +12,35 @@ const fst = require('../lib/FileSystemTools')
 program
     .option('-p, --port [port]', 'Port to use for push. Defaults to 4502.', /[0-9]+/, 4502)
     .option('-c, --credentials [user:password]', 'Credentials to use for push.')
+    .option('--sassonly', 'Only compile sass', false)
     .parse(process.argv)
 
 
 
 let credentials = program.credentials ? `--credentials ${program.credentials}` : ''
+let sassOnly = program.sassonly
 
-gulp.watch(['jcr_root/**/*','!jcr_root/**/.vlt', 'jcr_root/**/.content.xml'],
-    () => {
-        fst.pushToAEM(credentials, program.port)
-            .then(stdout => logAndNotifiy(stdout, 'success', 'both'))
-            .catch(err => logAndNotifiy(stdout, 'failure', 'both'))
-    }
-)
-    .on('change', event => console.log('File ' + event.path + ' was ' + event.type))
+if (!sassOnly) {
+    gulp.watch(['jcr_root/**/*','!jcr_root/**/.vlt', '!jcr_root/**/*.scss', 'jcr_root/**/.content.xml'],
+        () => {
+            fst.pushToAEM(credentials, program.port)
+                .then(stdout => logAndNotifiy(stdout, 'success', 'both'))
+                .catch(err => logAndNotifiy(stdout, 'failure', 'both'))
+        }
+    )
+        .on('change', event => console.log('File ' + event.path + ' was ' + event.type))
+}
 
 gulp.watch(['jcr_root/**/*.scss'],
-	() => {
-		gulp.src('jcr_root/**/*.scss')
-			.pipe(sass())
-			.on('error', sass.logError)
-			.pipe(gulp.dest('./jcr_root/'))
-	}
+    () => {}
 )
+    .on('change', (file) => {
+        gulp.src(file.path, {base: 'jcr_root'})
+            .pipe(sass())
+            .on('error', sass.logError)
+            .pipe(gulp.dest('jcr_root/'))
+            
+        logAndNotifiy([file.path,' was compiled'].join(''), 'success', 'both')
+    })
 
 logAndNotifiy("Gulp's watch has begun. Gulp is the shield that guards the realms of AEM.", 'info', 'both')
